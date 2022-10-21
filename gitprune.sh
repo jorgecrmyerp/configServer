371a9e6106f46aa7df0730657eb8de4348802a7f
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-10-21 16:30:22 +0200

    vi., 21 de oct. de 2022 16:30:202022-10-21 ++config client creados properties para configuracion cloud

--- gitprune.sh
+++ gitprune.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-10-21 creados properties para configuracion cloud"
+message="$(date)2022-10-21 ++config client creados properties para configuracion cloud"
 fi
 
 start=`date +%s`
