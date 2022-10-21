7c2a7bb42177a9e829c00b01b8f96faa5ceb246d
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-10-21 18:27:52 +0200

    vi., 21 de oct. de 2022 18:27:51 2022-10-21 modificados config

--- git.sh
+++ git.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date) 2022-10-17 PRIMERA SUBIDA DEL DIA"
+message="$(date) 2022-10-21 modificados config"
 fi
 
 start=`date +%s`
