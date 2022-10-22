f10796e0435ce2e83a49eb0909e6a4393cc67811
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-10-22 11:42:06 +0200

    sá., 22 de oct. de 2022 11:42:042022-10-22 CORREGIDO PROPERTIES GATEWAY PARA QUE ENTRE MICRO CONFIGURACION

--- gitprune.sh
+++ gitprune.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-10-22 GATEWAY SERVER FUNCIONA DESDE GIT"
+message="$(date)2022-10-22 CORREGIDO PROPERTIES GATEWAY PARA QUE ENTRE MICRO CONFIGURACION"
 fi
 
 start=`date +%s`
