91db04d4fbca3e294f8692b9ddee9a8220e3461e
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-10-22 10:41:18 +0200

    sá., 22 de oct. de 2022 10:41:17 2022-10-22 ++microservicio configuracion

--- basico.sh
+++ basico.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date) 2022-10-22 PROPERTIES PARA GATEWAY-SERVER"
+message="$(date) 2022-10-22 ++microservicio configuracion"
 fi
 
 git add .
