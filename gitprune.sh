f4c189740984541a8c6cfb119de5be51e932b412
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-10-23 14:01:17 +0200

    do., 23 de oct. de 2022 14:01:152022-10-23 PROBADO RepositoryRestResource

--- gitprune.sh
+++ gitprune.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-10-23 CAMBIADA BBDD H2 POR CURSOS"
+message="$(date)2022-10-23 PROBADO RepositoryRestResource"
 fi
 
 start=`date +%s`
