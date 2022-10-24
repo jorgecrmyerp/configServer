fae4d66ecc5634a048681809167acff3f8bd32d3
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-10-24 17:34:38 +0200

    lu., 24 de oct. de 2022 17:34:362022-10-24 ++no va resilience4j, ++zipkin

--- gitprune.sh
+++ gitprune.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-10-24 ++otra prueba resilience4j.circuitbreaker"
+message="$(date)2022-10-24 ++no va resilience4j, ++zipkin"
 fi
 
 start=`date +%s`
