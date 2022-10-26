f6225b29bc37a67f8c31719553427d974245b367
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-10-26 11:03:40 +0200

    mi., 26 de oct. de 2022 11:03:372022-10-26 microservicio examenes

--- gitprune.sh
+++ gitprune.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-10-26 primera subida del dia"
+message="$(date)2022-10-26 microservicio examenes"
 fi
 
 start=`date +%s`
