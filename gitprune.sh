510e86e62cec8eeb0a9c3e8b3437a27859b4552d
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-10-27 08:31:28 +0200

    ju., 27 de oct. de 2022  8:31:262022-10-27 RELACIONES ASIGNATURAS

--- gitprune.sh
+++ gitprune.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-10-27 PRIMERA SUBIDA DEL DIA"
+message="$(date)2022-10-27 RELACIONES ASIGNATURAS"
 fi
 
 start=`date +%s`
