d00bb4223c2985050316b7af49afc785c1b43db8
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-10-28 17:59:10 +0200

    vi., 28 de oct. de 2022 17:59:072022-10-28 lista respuestas

--- gitprune.sh
+++ gitprune.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-10-28 spring.jpa.hibernate.ddl-auto=update"
+message="$(date)2022-10-28 lista respuestas"
 fi
 
 start=`date +%s`
